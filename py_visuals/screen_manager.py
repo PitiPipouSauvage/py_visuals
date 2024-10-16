534526ff91958fa9929320d8ee1a20b1020c0839
PitiPipouSauvage <thomas.baert2014@gmail.com>
2024-10-16 11:06:41 +0200

    bug

--- py_visuals/screen_manager.py
+++ py_visuals/screen_manager.py
@@ -23,11 +23,8 @@ class Screen:
 
 	@staticmethod
 	def superimpose(base: np.array, overlay: mapping.Map, x_coord=0, y_coord=0) -> None:
-		print(overlay.map.shape)
-		print(base.shape)
 		for i in range(overlay.map.shape[0]):
 			for j in range(overlay.map.shape[1]):
-				print(j, i)
 				base[j + x_coord, i + y_coord] = overlay.map[i, j]		
 
 	def generate_screen(self, nb_layers: int, layers: list[tuple[tuple]]) -> np.array:
